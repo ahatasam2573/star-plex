32345f16d67f8f539a9cc3dcd39f0ddcab6ecbde
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2023-02-28 01:40:53 +0600

    custom stylesheet added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
+import './stylesheets/alignments.css'
+import './stylesheets/custom.css'
+import './stylesheets/formElements.css'
+import './stylesheets/sizes.css'
+import './stylesheets/theme.css'
+
 
 
 function App() {
--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Button } from 'antd';
 
 const Register = () => {
     return (
-        <div>Register</div>
+        <div className='flex justify-center h-screen items-center bg-secondary'>
+            <Button className='bg-primary text-white text-xl p-3 flex justify-center items-center'>Primary Button</Button>
+        </div>
     )
 }
 
