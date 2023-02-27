5e86ebc4c639ab937b590889019d8b92d79273ed
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2023-02-27 23:35:14 +0600

    folder sturcture completed for frotnted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Star Plex</h1>
-      <h2>Welcome</h2>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
     </div>
   );
 }
